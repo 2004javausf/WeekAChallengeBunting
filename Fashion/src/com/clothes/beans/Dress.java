36f89369d9f7808bfcd8a226c20d9e626a57b563
kib4885 <kib4885@gmail.com>
2020-04-20 16:23:21 -0400

    first

---
+++ Fashion/src/com/clothes/beans/Dress.java
@@ -0,0 +1,10 @@
+package com.clothes.beans;
+
+public class Dress {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Fashion/src/com/clothes/beans/Pants.java
@@ -0,0 +1,5 @@
+package com.clothes.beans;
+
+public class Pants {
+
+}
